003b5b2042193c3a0338c3d1a4eb8e9bcca1bba0
NathanG3 <npgalloway04@gmail.com>
2025-05-06 20:02:45 -0500

    first commit

--- Milestone1.cpp
+++ Milestone1.cpp
@@ -3,9 +3,55 @@
 //
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <queue>
 #include <vector>
 
 using namespace std;
+
+int main()
+{
+    string filename = "world_cities.csv";
+    string country_code, city_name;
+
+    cout << "Enter country code: ";
+    cin >> country_code;
+    cin.ignore();
+    cout << "Enter city name: ";
+    getline(cin, city_name);
+
+    ifstream file;
+    file.open(filename);
+
+    if (!file.is_open()) {
+        cerr << "Failed to open file: " << filename << endl;
+        return 1;
+    }
+    string line;
+    bool found = false;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string country, city, population;
+        if (getline(ss, country, ',') && getline(ss, city, ',') && getline(ss, population))
+        {
+            if (city == city_name && country == country_code)
+            {
+                cout << "Population: " << population <<
+                    "\nCity Name: " << city <<
+                        "\nCountry Code: " << country_code << endl;
+                found = true;
+                break;
+            }
+        }
+
+
+    }
+    if (!found) {
+        cout << "Not found";
+    }
+
+
+}
