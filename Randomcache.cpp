16b8b548a52543c3bee9d9571e7875cfa2de12ae
NathanG3 <npgalloway04@gmail.com>
2025-05-13 17:15:46 -0500

    Randomcache 1.0

---
+++ Randomcache.cpp
@@ -0,0 +1,46 @@
+//
+// Created by npgal on 5/13/2025.
+//
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <vector>
+#include <random>
+#include "Cache.cpp"
+using namespace std;
+
+class Randomcache : public Cache {
+  unordered_map<string,string> cache;
+  vector<string> keys;
+  public:
+    void addstring(string key, string value)
+    {
+      if(cache.size() >= 10)
+      {
+        int index = rand() % 9;
+        cache.erase(keys[index]);
+        keys.erase(keys.begin() + index);
+      }
+      cache[key] = value;
+      keys.push_back(key);
+    }
+    bool getstring(string& key, string& population)
+    {
+      if(cache.find(key) != cache.end())
+      {
+        population = cache[key];
+        return true;
+      }
+      return false;
+    }
+    void display()
+    {
+      for (const auto& entry : keys)
+      {
+        cout << entry << " : " << cache[entry] << endl;
+      }
+    }
+};
