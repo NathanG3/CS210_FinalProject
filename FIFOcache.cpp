63ab6556896a05a6de9dd1d3c3ee35b266dafe80
NathanG3 <npgalloway04@gmail.com>
2025-05-13 00:23:49 -0500

    FIFO 1.0

---
+++ FIFOcache.cpp
@@ -0,0 +1,52 @@
+//
+// Created by npgal on 5/12/2025.
+//
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class FIFOcache {
+  queue<string> q;
+  unordered_map<string, string> cache;
+
+  public:
+    void addstring(string& key, string& population)
+    {
+      if (cache.find(key) != cache.end())
+      {
+        if (q.size() >= 10){
+          string temp = q.front();
+          q.pop();
+          cache.erase(temp);
+        }
+        q.push(key);
+      }
+      cache[key] = population;
+    }
+
+    bool getstring(string& key, string& population)
+    {
+      if (cache.find(key) == cache.end())
+      {
+        population = cache[key];
+        return true;
+      }
+
+      return false;
+    }
+
+    void display()
+    {
+      for (const auto& entry : q)
+      {
+        cout << entry << " : " << cache[entry] << endl;
+      }
+    }
+
+};
